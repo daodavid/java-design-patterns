727081389373eb6840874f3a6a698cc424e06d6b
david <david.stankov@haemimont.com>
2019-02-21 13:39:00 +0200

    Bridge pattern

--- src/pattern/bridge/package-info.java
+++ src/pattern/bridge/package-info.java
@@ -1,7 +1,18 @@
 package pattern.bridge;
 
 /*
-      The bridge pattern's intent is to decouple abstraction from its implemantation so that the two can varry indepentatly
+      The bridge pattern's intent is to decouple abstraction from its               implemantation so that the two can varry indepentatly
          ir puts abstraction and implementation in two different class
          hierachies so that both can be extend independently
+
+    * Bridge pattern decouple the abstract classs and its implemtation
+    * classses by providing a bridge structute between them
+    * The bridge uses encapsulation,agregation ,and inhariance to separate
+    * responsibility into classes
+    * The bridge pattern is useful when both the class and what it does vary,often because changes in the class can be made easily
+
+           The bridge pattern avoid compile-time binding between abstraction and its implemntation
+           we can use this pattern to create abstraction between implemtor object in compile time ,and also we can change this implemtor dynamicaly
+
+
  */
