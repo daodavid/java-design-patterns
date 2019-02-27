eb5348a143851362808bf2002bb9aac42eb1a635
david <david.stankov@haemimont.com>
2019-02-27 11:57:41 +0200

    info

--- src/pattern/TemplateMethod/package-info.java
+++ src/pattern/TemplateMethod/package-info.java
@@ -3,8 +3,9 @@ package pattern.TemplateMethod;
 /*
    Template method pattern defines of skeleton of
    operation by some final method implement in
-   super abstract class wich will be execute details
-   wich will have been implemented  in child class
+   super abstract class wich will  execute details
+   in form of abstracted methods in specific order
+   implemented from child classes
 
    Thus we increase  the abstraction for client
  */
